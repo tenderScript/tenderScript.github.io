3b31fd903889ad3c9a25e1b05d5c11264186f743
Tony Ellison <tonyjellison@gmail.com>
2017-05-12 02:36:23 -0400

    quickfix animation length

--- js/animations.js
+++ js/animations.js
@@ -7,8 +7,8 @@ window.onload = function() {
 };
 
 var winWidth = document.documentElement.clientWidth;
-var startPath = "M-10 250 C 0 250, 1 250, 300 250 S 600 250, 1540 250";
-var startPath2 = "M-10 250 C 0 250, 600 250, 300 250 S 1 250, 1540 250";
+var startPath = "M-10 250 C 0 250, 1 250, 300 250 S 600 250, " + winWidth + " 250";
+var startPath2 = "M-10 250 C 0 250, 600 250, 300 250 S 1 250, " + winWidth + " 250";
 var grad3 = Snap('.wave3').gradient("l(0.5, 0, .5, 1)#3994E5-#8062FF-#9758FF");
 
 var wave1= Snap('.wave1').path([startPath]).attr({
@@ -49,11 +49,11 @@ function animateWave(waveNum, width, miliSec) {
   };
 
   if (waveNum == wave2 ) {
-    var Path1 = "M-10 275 C 0 275, 100 150, 300 175 S 500 250, " + length + " 275";
-    var Path2 = "M-10 300 C 0 300, 300 175, 500 150 S 700 300, " + length + " 275";
-    var Path3 = "M-10 275 C 0 275, 500 250, 700 175 S 900 150, " + length + " 300";
-    var Path4 = "M-10 275 C 0 275, 700 275, 900 200 S 1200 150, " + length + " 250";
-    var Path5 = "M-10 275 C 0 275, 900 250, 1100 200 S 1500 150, " + lengthEnd + " 150";
+    var Path1 = "M-10 275 C 0 275, 150 175, 400 175 S 600 250, " + length + " 275";
+    var Path2 = "M-10 300 C 0 300, 350 200, 700 175 S 1100 300, " + length + " 275";
+    var Path3 = "M-10 275 C 0 275, 550 225, 900 175 S 1200 175, " + length + " 300";
+    var Path4 = "M-10 275 C 0 275, 750 250, 1200 175 S 1400 175, " + length + " 275";
+    var Path5 = "M-10 275 C 0 275, 1150 300, 1400 300 S 1600 300, " + lengthEnd + " 300";
   };
 
   if (waveNum == wave3 ) {
@@ -99,18 +99,3 @@ function animateWave(waveNum, width, miliSec) {
     animateWave(waveNum, width, miliSec);
   }, time * 6)
 };
-
-/*improving
-function animatePath(waveNum, pathObj, miliSec) {
-
-  for (var i = 0; i < pathObj.length; i++) {
-    setTimeout(function(){
-      waveNum.animate({ d: pathObj[i] }, miliSec);
-    }, (miliSec * i));
-  };
-
-  return setTimeout(function(){
-    animatepath(waveNum, pathObj, miliSec);
-  }, (miliSec * (pathObj.length + 1)))
-};
-*/
